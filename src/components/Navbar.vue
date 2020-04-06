d6fe9908c4f44afb9efab2a7047e26d3d00c3ce9
David Chen <chen7david@me.com>
2020-04-06 12:48:35 +0800

    update

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app-bar app dark>
+        <span>Identiy-Manager</span>
+        <v-spacer></v-spacer>
+        <v-btn tile>login</v-btn>
+        <v-btn tile>register</v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- src/store/modules/main.js
+++ src/store/modules/main.js
@@ -1,36 +1,15 @@
 
 export default {
   state: {
-    errors: null,
-    snackbar: {},
-    navbar: {}
+
   },
   getters: {
-    errors: (state) => state.errors,
-    validate: (state) => (key) => {
-      let error =  state.errors ? state.errors.find(el => el.key == key) : null
-      return error ? error.message : ''
-    },
-    snackbar: (state) => state.snackbar,
-    navbar: (state) => state.navbar 
+
   },
   mutations: {
-    SET_ERRORS: (state, errors) => state.errors = errors,
-    RESET_ERRORS: (state) => state.errors = [],
-    SET_SNACKBAR: (state, detail) => state.snackbar = {
-      show: true,
-      text: detail.message,
-      color: detail.state || 'dark'
-    },
+
   },
   actions: {
-    getValidationErrors: ({commit}, errors) => {
-        commit('SET_ERRORS', errors)
-    },
 
-    setSnackbar: ({commit}, message) => {
-        commit('SET_SNACKBAR', message)
-        commit('RESET_ERRORS')
-    }
   },
 }
