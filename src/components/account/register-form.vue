cc4f3eaeda2a62447a370a1e562efe541b55e4c5
David <chen7david@me.com>
2020-04-06 14:30:27 +0800

    update

--- src/components/account/register-form.vue
+++ src/components/account/register-form.vue
@@ -1,14 +1,33 @@
 <template>
-  <v-card>
-    <v-col cols="12" md="3" sm="6">
+  <v-card max-width="400px">
+    <v-col cols="12">
         <v-list-item two-line>
           <v-list-item-content>
               <div class="overline">register</div>
-              <v-list-item-subtitle>hh</v-list-item-subtitle>
+              <v-list-item-subtitle>create an account today</v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>  
-    </v-col>
+        <v-col>
+          <v-text-field
+            label="username"
+            name="username"
+            outlined
+          ></v-text-field>
+        </v-col>
 
+        <v-col>
+          <v-text-field
+            label="password"
+            name="password"
+            outlined
+            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show ? 'text' : 'password'"
+            hint="At least 6 characters"
+            @click:append="show = !show"
+          ></v-text-field>
+        </v-col>
+        <v-btn outlined block>register</v-btn>
+    </v-col>
   </v-card>
 </template>
 
@@ -16,7 +35,7 @@
   export default {
     name: 'register-form',
     data: () => ({
-      
+      show: false
     }),
   }
 </script>
