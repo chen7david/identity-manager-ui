8e37d60e81bccfbbb412c295c12cb7f1b22a4dce
David Chen <chen7david@me.com>
2020-04-06 11:01:29 +0800

    update

---
+++ src/plugins/http.js
@@ -0,0 +1,32 @@
+const axios = require('axios')
+import store from '../store'
+
+const http = axios.create({
+    baseURL: 'http://192.168.50.124:4000',
+    timeout:6000,
+})
+
+http.interceptors.request.use(config => {
+    config.url = encodeURI(config.url)
+    let accessToken = localStorage.getItem('access-token')
+    if(accessToken != null) config.headers.Authorization = `Bearer ${accessToken}`;
+    return config
+
+}, error =>{
+    console.log({'req-error': error})
+    return Promise.reject(error)
+})
+
+http.interceptors.response.use(response => {
+    const { isCargo, payload, details } = response.data
+    console.log(response.data)
+    response.data = payload
+    return response
+}, error =>{
+
+    const data = error.response && error.response.data    
+
+    return Promise.reject(error)
+})
+
+export default http
