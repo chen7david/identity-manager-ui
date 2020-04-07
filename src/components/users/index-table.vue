89cab78cde4c8d65b25abd50984734e4f840340f
David <chen7david@me.com>
2020-04-07 11:45:55 +0800

    update

---
+++ src/components/users/index-table.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-data-table
+        :headers="headers"
+        :items="users"
+        :search="search"
+        :items-per-page="5"
+        class="elevation-1"
+    >
+        <template v-slot:item.actions="{ item }">
+            <v-icon small class="mr-2" @click="seeItem(item)">mdi-pencil</v-icon>
+            <v-icon small @click="seeItem(item)">mdi-delete</v-icon>
+        </template>
+    </v-data-table>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'Users',
+  props: {
+    users: null,
+    search: null
+  },
+  data () {
+    return {
+        headers: [
+            {
+            text: 'Username',
+            align: 'start',
+            sortable: false,
+            value: 'username',
+            },
+            { text: 'UserId', value: 'userId' },
+            { text: 'Last Updated', value: 'updated_at' },
+            { text: 'Actions', value: 'actions', sortable: false },
+        ],
+    }
+  },
+  methods: {
+      ...mapActions([
+          'getUsers'
+      ]),
+      seeItem(item){
+          console.log(item)
+      }
+  },
+}
+</script>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,54 +1,17 @@
 <template>
   <v-container fill-height fluid>
-      
-        <v-text-field
-            v-model="search"
-            append-icon="mdi-magnify"
-            label="Search"
-            single-line
-            hide-details
-        ></v-text-field>
-
-        <v-data-table
-            :headers="headers"
-            :items="users"
-            :search="search"
-            :items-per-page="5"
-            class="elevation-1"
-        >
-
-        <template v-slot:item.actions="{ item }">
-            <v-icon small class="mr-2" @click="seeItem(item)">mdi-pencil</v-icon>
-            <v-icon small @click="seeItem(item)">mdi-delete</v-icon>
-        </template>
-
-        </v-data-table>
+      <IndexTable :users="users"/>
   </v-container>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import IndexTable from './../components/users/index-table'
 
 export default {
   name: 'Users',
   components: {
-    
-  },
-  data () {
-    return {
-        search:'',
-        headers: [
-            {
-            text: 'Username',
-            align: 'start',
-            sortable: false,
-            value: 'username',
-            },
-            { text: 'UserId', value: 'userId' },
-            { text: 'Last Updated', value: 'updated_at' },
-            { text: 'Actions', value: 'actions', sortable: false },
-        ],
-    }
+    IndexTable
   },
   computed: {
       ...mapGetters([
@@ -59,9 +22,6 @@ export default {
       ...mapActions([
           'getUsers'
       ]),
-      seeItem(item){
-          console.log(item)
-      }
   },
   mounted(){
       this.getUsers()
